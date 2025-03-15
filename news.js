943d713e26b387042afbd3d8c9bde0aacb9cf5c6
Priyal Sarda <priyal.sarda2005@gmail.com>
2025-03-15 16:54:58 +0530

    made few changes

---
+++ news.js
@@ -0,0 +1,77 @@
+const apiKey = `cac3f2da3dfc46c4a70820671ce33d60`
+const baseUrl = `https://newsapi.org/v2`
+
+const contentWrapper = document.getElementById("contentWrapper");
+const newsSearch = document.getElementById("newsSearchInput");
+
+
+async function getNews(){
+    try{
+        const getNewsDataApi = await fetch (`${baseUrl}/top-headlines?country=us&apiKey=${apiKey}`)
+        const data = await getNewsDataApi.json()
+        return data;
+    }
+    catch (error){
+        console.log(error)
+    }
+}
+
+getNews().then(data => renderNews(data.articles));
+
+function renderNews(newsData) {
+    newsData.forEach(news => {
+        const defualtImage = `https://picsum.photos/600`
+        const data = {
+            // ?? means that when the data is null, it becomes defualtImage instead
+            urlImage: news.urlToImage  ?? defualtImage,
+            date: news.publishedAt,
+            title: news.title,
+            description: news.description ?? "",
+            url: news.url
+        }
+
+        const card = `
+        <div class="card">
+                <div class="card-image-wrapper">
+                    <img src="${data.urlImage}">
+                </div>
+                <div class="card-content">
+                    <span class="card-date">${data.date}</span>
+                    <h2 class="card-title">
+                        <a href="${data.url}">${data.title}</a>
+                    </h2>
+                    <p class="card-description">${data.description}</p>
+                </div>
+            </div>
+        `
+
+        // Append the card to the content wrapper
+        contentWrapper.insertAdjacentHTML('beforeend', card)
+    });
+}
+
+
+async function getSearchNews(query){
+    try{
+        const getNewsDataApi = await fetch (`${baseUrl}everything?q=${query}/&apiKey=${apiKey}`)
+        const data = await getNewsDataApi.json()
+        return data;
+    }
+    catch (error){
+        console.log(error)
+    }
+}
+
+
+// Event listener for searching news
+newsSearch.addEventListener("input", event => {
+    const inputSearchValue = event.target.value;
+    contentWrapper.innerHTML = ""
+    if (inputSearchValue == ""){
+        getNews().then(data => renderNews(data.articles));
+    }
+    else{
+        getSearchNews(inputSearchValue).then(
+            data => renderNews(data.articles))
+    }
+});
